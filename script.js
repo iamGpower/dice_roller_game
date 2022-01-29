97888e8401ec1c733721c625b85792617fe04d34
GP <iamgpower3@gmail.com>
2022-01-29 15:59:17 +0100

    implemented active player switching 🚀

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const currentScore0El = document.querySelector('#current--0');
 const currentScore1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
@@ -17,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 const rollDice = () => {
   // a. Generate a random dice roll
@@ -31,9 +35,17 @@ const rollDice = () => {
   if (randomDiceRoll !== 1) {
     // Add dice to current score
     currentScore += randomDiceRoll;
-    currentScore0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // currentScore0El.textContent = currentScore;
   } else {
     // Switch to next player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 };
 
